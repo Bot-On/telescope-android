60a3d2fada8cf14a3bc714a9428006022815996f
jcamarero <jcamarero@quadram.mobi>
2018-07-31 13:15:57 +0200

    modify init to request permissions

--- app/src/main/java/com/b4motion/geolocation/geolocation/core/GeoLocation.kt
+++ app/src/main/java/com/b4motion/geolocation/geolocation/core/GeoLocation.kt
@@ -1,21 +1,48 @@
 package com.b4motion.geolocation.geolocation.core
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import com.b4motion.geolocation.geolocation.globals.log
 
 /**
  * Created by frodriguez on 7/30/2018.
  *
  */
-class GeoLocation constructor(private val applicationContext: Context){
+class GeoLocation constructor(private val activity: AppCompatActivity){
 
     fun init(deviceId: String){
+        checkPermissions()
         try {
-            GeoB4.getInstance().init(applicationContext, deviceId)
+            GeoB4.getInstance().init(activity, deviceId)
         } catch (e: SecurityException) {
             log("que no tienes permisos!!!!")
         }
     }
 
+    private fun checkPermissions() {
+        var permissions = mutableListOf<String>()
+
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.CALL_PHONE)
+
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.READ_PHONE_STATE)
+
+        var permissionsToRequest = Array(permissions.size) { "" }
+
+        for (i in 0 until permissions.size) {
+            permissionsToRequest[i] = permissions[i]
+        }
+
+        if (permissionsToRequest.isNotEmpty())
+            ActivityCompat.requestPermissions(activity, permissionsToRequest, 0)
+    }
 
 }
